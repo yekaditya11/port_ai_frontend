fb80b93b82ccb542c5fce9491cdda09176868b55
Mohammad Hassaan <mdhassan@gytworkz.com>
2026-04-06 10:51:56 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import IncidentListing from './components/incident/IncidentListing';
 import IncidentReview from './components/incident/IncidentReview';
 import IncidentInspection from './components/incident/IncidentInspection';
 import IncidentInvestigation from './components/incident/IncidentInvestigation';
+import RootCauseAnalysis from './components/incident/RootCauseAnalysis';
+import IncidentResolution from './components/incident/IncidentResolution';
+import IncidentAction from './components/incident/IncidentAction';
+import IncidentWorkflow from './components/incident/IncidentWorkflow';
+import ObservationDashboard from './components/observation/ObservationDashboard';
+import CreateObservation from './components/observation/CreateObservation';
+import ObservationListing from './components/observation/ObservationListing';
+import ObservationInspection from './components/observation/ObservationInspection';
+import ObservationClosure from './components/observation/ObservationClosure';
+import ObservationReview from './components/observation/ObservationReview';
+import ObservationAction from './components/observation/ObservationAction';
+import ObservationRCA from './components/observation/ObservationRCA';
+import ObservationAnalyzer from './components/observation/ObservationAnalyzer';
+import ObservationWorkflow from './components/observation/ObservationWorkflow';
+import ObservationLog from './components/observation/ObservationLog';
+import ObservationConfiguration from './components/observation/ObservationConfiguration';
+import DensityReport from './components/observation/reports/DensityReport';
+import ListReport from './components/observation/reports/ListReport';
 
 function App() {
   return (
@@ -22,6 +40,24 @@ function App() {
           <Route path="/incident/review" element={<IncidentReview />} />
           <Route path="/incident/inspection-listing" element={<IncidentInspection />} />
           <Route path="/incident/investigation" element={<IncidentInvestigation />} />
+          <Route path="/incident/root-cause-analysis" element={<RootCauseAnalysis />} />
+          <Route path="/incident/resolution" element={<IncidentResolution />} />
+          <Route path="/incident/action" element={<IncidentAction />} />
+          <Route path="/incident/workflow" element={<IncidentWorkflow />} />
+          <Route path="/observation/dashboard" element={<ObservationDashboard />} />
+          <Route path="/observation/create-new-observation" element={<CreateObservation />} />
+          <Route path="/observation/listing" element={<ObservationListing />} />
+          <Route path="/observation/inspection" element={<ObservationInspection />} />
+          <Route path="/observation/closure" element={<ObservationClosure />} />
+          <Route path="/observation/review" element={<ObservationReview />} />
+          <Route path="/observation/action" element={<ObservationAction />} />
+          <Route path="/observation/root-cause" element={<ObservationRCA />} />
+          <Route path="/observation/analyser" element={<ObservationAnalyzer />} />
+          <Route path="/observation/workflow" element={<ObservationWorkflow />} />
+          <Route path="/observation/observation-log" element={<ObservationLog />} />
+          <Route path="/observation/reports/density-report" element={<DensityReport />} />
+          <Route path="/observation/reports/observation-list" element={<ListReport />} />
+          <Route path="/observation/configuration" element={<ObservationConfiguration />} />
         </Routes>
       </DashboardLayout>
     </BrowserRouter>
